d6017e61290d24ffbd640b354e989ec95e063ae8
Irakli <teliairakli@gmail.com>
2022-07-29 16:35:48 +0400

    HomeWork

---
+++ Lecture1.java
@@ -0,0 +1,37 @@
+package AcademyHomeWorks;
+
+public class Lecture1 {
+    private static int sum(int firstNum, int secondNum) {
+        int result1 = firstNum + secondNum;
+            return result1;
+    }
+    private static int subtraction(int fitstNum,int secondNum){
+        int result2 = fitstNum - secondNum;
+        return result2;
+    }
+    private static int multiply(int fitstNum,int secondNum){
+        int result3 = fitstNum * secondNum;
+        return result3;
+    }
+    private static int divide(int fitstNum,int secondNum){
+        int result4 = fitstNum / secondNum;
+        return result4;
+
+    }
+    public static void main(String[] args) {
+        int result1 = 76 + 23;
+        int result2 = 76 - 23;
+        int result3 = 76 * 23;
+        int result4 = 76 / 23;
+            System.out.println("შემოყვანილი ორი რიცხვის ჯამი უდრის : " + result1);
+            System.out.println("შემოყვანილი ორი რიცხვის სხვაობა უდრის: " + result2);
+            System.out.println("შემოყვანილი ორი რიცხვის ნამრავლი უდრის: " + result3);
+            System.out.println("შემოყვანილი ორი რიცხვის გაყოფა უდრის: " + result4);
+
+
+
+    }
+
+
+
+}
